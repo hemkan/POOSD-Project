b91ab58f0d5400352897d183a75c297cf76234e5
jjoslin0994 <jjoslin0994@gmail.com>
2023-09-20 20:07:53 -0400

    modifictions to work with mySQL. Now when a user registers, the database is updated the 'users' table

--- pages/RegistrationController.php
+++ pages/RegistrationController.php
@@ -1,10 +1,17 @@
 <?php
 // RegistrationController.php
-require_one('/var/www/html/config/mysqlConfig.php');
+require_once('/var/www/html/config/mysqlConfig.php');
 require_once('/var/www/html/config/database.php');
 
 class RegistrationController
 {
+    private $pdo; // PDO instance
+
+    public function __construct($pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
     public function registerUser($postData)
     {
         // Validate user input: username and password are required
@@ -15,22 +22,40 @@ class RegistrationController
         // Hash the user's password using PHP's built-in password hasher
         $hashedPassword = password_hash($postData['password'], PASSWORD_DEFAULT);
 
+        // Prepare SQL statement to insrt user into database
+        $statement = $this->pdo->prepare("INSERT INTO users (username, hashed_password) VALUES (:username, :hashed_password)");
+
+        // Bind parameteres
+        $statement->bindParam(':username', $postData['username']);
+        $statement->bindParam(':hashed_password', $hashedPassword);
+
+        // Exacute SQL statement
+        try {
+            $statement->execute();
+            return ['success' => 'User registered successfully.'];
+        }catch(PDOException $e) {
+            // handle database insertion error
+            return['error' => 'Registration failed. ' . $e->getMessage()];
+        }
+    }
+}
+        /*
         // Simulate saving user data (you should save it to a database in a real application)
         $userData = [
             'username' => $postData['username'],
             'hashed_password' => $hashedPassword
         ];
+        */
+// Create PDO instance
 
-        // Return the user data as a success response (in a real application, you might also generate a token or session)
-        return ['success' => 'User registered successfully.', 'user_data' => $userData];
-    }
-}
+$pdo = new PDO('mysql:host=localhost;dbname=SLP-7_DB', 'webuser', '25WindmillCat71323');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-// Example usage:
-$controller = new RegistrationController();
-$postData = ['username' => 'jon joslin', 'password' => 'examplePassword'];
+// Example:
+$controller = new RegistrationController($pdo);
+$postData = ['username' => 'jonj4040', 'password' => 'examplePassword'];
 $result = $controller->registerUser($postData);
 
-// Output the result (you should handle the response accordingly in your application)
+// Output 
 echo json_encode($result);
 ?>

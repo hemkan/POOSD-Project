69665aea5d232cd51b99f9f9f6b9f672c69ba664
jjoslin0994 <jjoslin0994@gmail.com>
2023-09-26 14:35:23 -0400

    perfroms login and creates instance. Will reidrect to user page Instance holds the user data that can be used for access to contact database.

--- pages/LoginController.php
+++ pages/LoginController.php
@@ -1,28 +1,72 @@
 <?php
-// LoginContoller.php
 require_once 'PerformLogin.php';
+require_once '../config/mysqlConfig.php';
+require_once '../config/database.php'; // Adjust the path as needed
 
-class LoginContoller
+class LoginController
 {
-    public function loginUser($postData){
-        // validat user input
-        if(empty($postData['username']) || empty($postData['password'])){
-            return ['error' => 'User name and password required'];
-        }else {
-            // search database for name & password combo
-            $user = $this->retrieveUser($postData['username']);
+    private $db;
 
-            if($user && password_verify($postData['password'], $user['password'])){
-                // user name and password match
-                PerformLogin::performLogIn($user);
-            }else{
-                //
-                return ['error' => 'User name and password pair not found'];
-            }
+    public function __construct(PDO $db)
+    {
+        $this->db = $db;
+    }
+
+    public function loginUser($postData)
+    {
+        if (empty($postData['username']) || empty($postData['password'])) {
+            return ['error' => 'Username and password are required'];
+        }
+
+        $user = $this->retrieveUser($postData['username']);
+
+        if (!$user || !password_verify($postData['password'], $user['password'])) {
+            throw new Exception('Invalid username or password.');
+        }
+
+        PerformLogin::performLogIn($user);
+
+        return ['success' => 'Login successful'];
+    }
+
+    public function retrieveUser($username)
+    {
+        try {
+            $statement = $this->db->prepare("SELECT * FROM users WHERE username = :username");
+            $statement->bindParam(':username', $username, PDO::PARAM_STR);
+            $statement->execute();
+
+            $user = $statement->fetch(PDO::FETCH_ASSOC);
+
+            return $user;
+        } catch (PDOException $e) {
+            throw new Exception('Database query error: ' . $e->getMessage());
         }
     }
-    public function retrieveUser($postData){
-        // search database for user by enterd name
+}
+
+try {
+    $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
+    $loginController = new LoginController($pdo);
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        // Assuming you have POST data for username and password
+        $postData = [
+            'username' => $_POST['username'],
+            'password' => $_POST['password'],
+        ];
+
+        $result = $loginController->loginUser($postData);
+
+        if (isset($result['error'])) {
+            echo $result['error'];
+        } elseif (isset($result['success'])) {
+            echo $result['success'];
+        }
     }
+} catch (PDOException $e) {
+    echo 'Database connection error: ' . $e->getMessage();
+} catch (Exception $e) {
+    echo 'Login error: ' . $e->getMessage();
 }
 ?>
--- pages/PerformLogin.php
+++ pages/PerformLogin.php
@@ -1,8 +1,15 @@
 <?php
 // PerformLogIn.php
-class PerformLogIn{
-    public function performLogIn($user){
-
+class PerformLogIn
+{
+    public function __construct($user) 
+    {
+        // start session (if not already started)
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+        $_SESSION['user'] = $user;
     }
+    
 }
 ?>

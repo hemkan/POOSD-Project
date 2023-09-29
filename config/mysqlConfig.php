7c1ec4e48a8d5caa5f77e83989b68f9009ce0a15
jjoslin0994 <jjoslin0994@gmail.com>
2023-09-29 18:41:50 -0400

    Add .gitkeep files to new directories

--- config/mysqlConfig.php
+++
@@ -1,8 +0,0 @@
-<?php
-// mysqlConfig.php
-
-define('DB_HOST', 'localhost'); // host where mysql is running 
-define('DB_USER', 'webuser');
-define('DB_PASS', '25WindmillCat71323');
-define('DB_NAME', 'SLP-7_DB');
-?>

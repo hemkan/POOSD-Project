a62a65d810fbdfc1ad692ee1e06b8fed05ec568e
hemkan <hemkano1403@gmail.com>
2023-10-07 18:00:08 -0400

    make create/edit compatible on smaller screens

--- pages/crud.php
+++ pages/crud.php
@@ -84,7 +84,7 @@ if(!isset($_SESSION['user']))
                 height: 100%;
                 background-color: rgb(255, 255, 255);
                 z-index: 999;
-                transition: .3s;
+                transition: opacity 0.3s;
             }
             #heading {
                 text-transform: uppercase;
@@ -96,6 +96,21 @@ if(!isset($_SESSION['user']))
             #invalidMessage {
                 color: red;
             }
+            #ECForm {
+                width: 60vh;
+            }
+            @media (max-width: 450px) {
+                .createBtn {
+                    width: 100%;
+                }
+                #ECForm {
+                    width: 100%;
+                    max-width: 100%;
+                }
+
+                .form-control {
+                    width: 100%;
+                }
         </style>
         <!-- <script type="module" src="../js/search.js"></script> -->
         
@@ -123,7 +138,7 @@ if(!isset($_SESSION['user']))
                             <a href="../index.html" id="loginC" class="btn ml-auto">Logout</a>
                         </nav>
                         <div class="container mt-5">
-                            <form class="container mt-5" style="width: 60vh;">
+                        <form class="container mt-5" id="ECForm">
                                 <h1 id="heading">Create Contact</h1>
                                 <div class="form-row">
                                     <div class="form-group col-md-6">

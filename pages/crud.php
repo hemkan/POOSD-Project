2f36553835bb4303619bab8cb575b705690e4537
hemkan <hemkano1403@gmail.com>
2023-10-06 21:17:37 -0400

    fixed import of display.js

--- pages/crud.php
+++ pages/crud.php
@@ -94,7 +94,8 @@ if(!isset($_SESSION['user']))
                 text-align: center;
             }
         </style>
-        <script type="module" src="../js/search.js"></script>
+        <!-- <script type="module" src="../js/search.js"></script> -->
+        
 
     </head>
     <body>
@@ -192,9 +193,11 @@ if(!isset($_SESSION['user']))
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
         <script src="../js/createContactHandler_slp7.js"></script>
         <script src="../js/display.js"></script>
+        <script src="../js/search.js"></script>
+        
        
 
-        </script>
+    <!-- </script> -->
     </body>
 
 </html>

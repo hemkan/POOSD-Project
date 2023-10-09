a34570696af501518e392b9a1b3af9f95a580019
hemkan <hemkano1403@gmail.com>
2023-10-08 21:22:52 -0400

    removed comments

--- pages/crud.php
+++ pages/crud.php
@@ -12,19 +12,16 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="/css/crud_styles.css">
         <script src="https://kit.fontawesome.com/5d8ee741cc.js" crossorigin="anonymous"></script>        
-
     </head>
     <body>
+        <!-- Navbar with logout option -->
         <header>
             <nav class="navbar navbar-expand-lg">
                 <a class="navbar-brand" href="#"><img src="/images/logo.png" alt="Home" width="50px"></a>
                 <div class="dropdown_L ml-auto">
-                    <!-- <button class="dropdown-toggle btn ml-auto" type="button" id="logoutC" data-toggle="dropdown_L2" aria-haspopup="true" aria-expanded="false"></button> -->
                     <a href="#" id="logout" class="btn ml-auto dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Name</a>
                     <div class="dropdown-menu dropdown-menu-right-custom" aria-labelledby="logout">
-                        <!-- <a class="dropdown-item" id="login" href="#">Logout</a> -->
                         <a href="/api/logout.php" id="login" class="dropdown-item">Logout</a>
-
                     </div>
                 </div>
             </nav>
@@ -36,11 +33,11 @@
                 </div>
                 <div class="col-auto">
                     <button id="create" class="icon"><i class="fa-solid fa-plus"></i></button>
+                    <!-- overlay for create and edit -->
                     <div class="overlay" style="opacity: 0;">
                         <nav class="navbar navbar-expand-lg navOverlay">
                             <a class="" id="close"><i class="fa-solid fa-chevron-left"></i></a>
                             <div class="dropdown_L2 ml-auto">
-                                <!-- <button class="dropdown-toggle btn ml-auto" type="button" id="logoutC" data-toggle="dropdown_L2" aria-haspopup="true" aria-expanded="false"></button> -->
                                 <a href="#" id="logoutC" class="btn ml-auto dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Name</a>
                                 <div class="dropdown-menu dropdown-menu-right-custom" aria-labelledby="logoutC">
                                     <a class="dropdown-item" id="loginC" href="/api/logout.php">Logout</a>
@@ -70,21 +67,23 @@
                                 </div>
                                 <div>
                                     <small id="invalidMessage" style="display: none;">Signup failed. Please try again.</small>
-                                <!-- <br> -->
                                 </div>
                                 <center><button class="text-center btn" id="createBtn" type="submit">Save</button></center>
                             </form>
                         </div>
                     </div>
+                    <!-- search bar with icon funcionality in full screen view -->
                     <span id="search-bar-container">
                         <button class="icon" id="search-icon"><i class="fa-solid fa-search"></i></button>
                         <input id="search-bar-input" type="text" placeholder="Search">
                     </span>
                 </div>
             </div>
+            <!-- search bar in small screen view -->
             <div class="row-auto">
                 <input id="search-bar-input-stacked" type="text" placeholder="Search">
             </div>
+            <!-- table section-->
             <div class="table-responsive">
                 <table class="table">
                     <thead>
@@ -101,6 +100,7 @@
                     </tbody>
                 </table>
             </div>
+            <!-- delete dialogue -->
             <div class="modal fade" id="confirmationModal" tabindex="-1" role="dialog" aria-labelledby="confirmationModalLabel" aria-hidden="true">
                 <div class="modal-dialog" role="document">
                     <div class="modal-content">
@@ -128,22 +128,18 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
         <script>
             function getUserName () {
-            const users_firstName = "<?php echo $_SESSION['user']['first_name']; ?>";
-            const capitalizeFirstLetter = users_firstName.charAt(0).toUpperCase() + users_firstName.slice(1);
-            return capitalizeFirstLetter;
-        
+                const users_firstName = "<?php echo $_SESSION['user']['first_name']; ?>";
+                const capitalizeFirstLetter = users_firstName.charAt(0).toUpperCase() + users_firstName.slice(1);
+                return capitalizeFirstLetter;
             }
         </script>
+        <!-- Helper JS files -->
         <script src="../js/createContactHandler_slp7.js"></script>
         <script src="../js/search.js"></script>
         <script src="../js/editContactHandler.js"></script>
+        <!-- Main JS file -->
         <script src="../js/display.js"></script>
-
-        
-       
-
-
     </body>
-
 </html>
+
 
